452583505c3a8a560930a7e977f4f185e41ccf0d
AlforovaNata <ndalferova@gmail.com>
2022-10-28 11:30:23 +0200

    Added axios integration so it shows the weather of the city

---
+++ src/Weather.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayForecast(response) {
+    setCity();
+    setLoading(true);
+    setWeather({
+      city: response.data.name,
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: Math.round(response.data.wind.speed),
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "c45c6ce7066c8be876b5001b7f84d122";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <div className="Search">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Type a city..."
+          onChange={updateCity}
+        />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
+
+  if (loading) {
+    return (
+      <div>
+        {form}
+        <ul className="Weather" type="none">
+          <h2>{weather.city}</h2>
+          <li>Temperature: {weather.temperature} °C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity} %</li>
+          <li>Wind: {weather.wind} m/sec</li>
+          <img src={weather.icon} alt={weather.description} />
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
