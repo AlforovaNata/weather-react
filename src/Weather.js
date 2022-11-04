b510b7c2f27bc82e82812dfd70a362c281df3134
AlforovaNata <ndalferova@gmail.com>
2022-11-04 18:07:35 +0100

    Forecast-Api

--- src/Weather.js
+++ src/Weather.js
@@ -9,15 +9,17 @@ export default function Weather(props) {
   const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
+    //console.log(response.data);
     setWeatherData({
       ready: true,
+      coord: response.data.coordinates,
       city: response.data.city,
       date: new Date(response.data.time * 1000),
       temperature: Math.round(response.data.temperature.current),
       description: response.data.condition.description,
       humidity: response.data.temperature.humidity,
       wind: Math.round(response.data.wind.speed),
-      icon: response.data.condition.icon_url,
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
     });
   }
 
@@ -32,6 +34,7 @@ export default function Weather(props) {
   function search() {
     let apiKey = "dc87a0f910f83fb8t9bb19co1299b4ab";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    
     axios.get(apiUrl).then(handleResponse);
   }
   if (weatherData.ready) {
@@ -59,7 +62,7 @@ export default function Weather(props) {
         </form>
 
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coord={weatherData.coord} />
       </div>
     );
   } else {
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,14 +1,28 @@
+import axios from "axios";
 import React from "react";
 import "./WeatherForecast.css";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  console.log(props);
+
+  let apiKey = "dc87a0f910f83fb8t9bb19co1299b4ab";
+  let units = "metric";
+  let longitude = props.coordinates.longitude;
+  let latitude = props.coordinates.latitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-day">Thu</div>
           <div className="WeatherForecast-icon">
-            <img src="#" alt="Cloud" size={38} />
+            <img src={props.data.condition.icon_url} alt="icon" />
           </div>
           <div className="WeatherForecast-temperature">
             <span className="WeatherForecast-temperature-max">19°</span>
